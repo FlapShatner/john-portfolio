41a652f2aa68efee2c52ed4c7a16ebd21d7fc7f6
Jordan Roberts <jrobertswebdev@gmail.com>
2023-05-02 23:59:53 -0500

    scroll lock

--- components/menu.tsx
+++ components/menu.tsx
@@ -1,7 +1,9 @@
 import { motion, AnimatePresence } from 'framer-motion'
 import { links } from '@/lib/links'
 import Link from 'next/link'
+import { useScrollLock } from '@/hooks/useScrollLock'
 import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 export type MenuProps = {
   show: boolean
@@ -12,12 +14,22 @@ const Menu = ({ show, setShow }: MenuProps) => {
   const router = useRouter()
   const { pathname } = router
 
+  const { lockScroll, unlockScroll } = useScrollLock()
+
+  useEffect(() => {
+    if (show) lockScroll()
+
+    return () => {
+      unlockScroll()
+    }
+  })
+
   return (
     <AnimatePresence>
       {show && (
         <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
-          <div className='absolute bg-black text-white top-0 left-0 h-full w-full flex justify-center items-start transition-all'>
-            <div className='flex flex-col gap-8 text-3xl font-semibold pt-40'>
+          <div className='absolute bg-black text-white top-0 left-0 h-full w-full flex justify-center items-start transition-all '>
+            <div className='flex flex-col gap-8 text-3xl font-semibold pt-20'>
               {links.map((link) => (
                 <Link key={link.linkName} href={link.linkPath}>
                   <div className={`${pathname == link.linkPath ? 'text-red-600' : 'text-white'}`} onClick={() => setShow(!show)}>
