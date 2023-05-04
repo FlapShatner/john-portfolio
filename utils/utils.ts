14e1dfa073a2f7dd56907d4b6cf24d9d04d9d41f
Jordan Roberts <jrobertswebdev@gmail.com>
2023-05-04 02:00:02 -0500

    console log instead of throw error in getNeighbors

--- utils/utils.ts
+++ utils/utils.ts
@@ -2,7 +2,7 @@ import { CoversImagesArray } from '@/lib/covers'
 
 export function getNeighbors(arr: CoversImagesArray, index: number) {
   if (!Array.isArray(arr) || arr.length < 3 || index < 0 || index >= arr.length) {
-    throw new Error('Invalid input')
+    console.log('Invalid input')
   }
   const length = arr.length
   const prevIndex = (index - 1 + length) % length
