62d02b35099563f25b687102f69550ac99204bc2
Jordan Roberts <jrobertswebdev@gmail.com>
2023-05-10 13:07:43 -0500

    adjust social icons color

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,10 +5,10 @@ export const Footer = () => {
   return (
     <div className='flex justify-center pt-6 pb-16 gap-4'>
       <Link href='https://www.linkedin.com/in/john-roberts-1b5ba23a/'>
-        <FaLinkedin className='text-black hover:text-gray-300' size={56} />
+        <FaLinkedin className='hover:text-[var(--fg)] text-gray-400' size={56} />
       </Link>
       <Link href='https://www.instagram.com/johnwillumroberts/'>
-        <FaInstagramSquare className='text-black hover:text-gray-300' size={56} />
+        <FaInstagramSquare className='hover:text-[var(--fg)] text-gray-400' size={56} />
       </Link>
     </div>
   )
